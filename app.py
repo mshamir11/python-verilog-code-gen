42fa2b348fff0a671208c4a4381ed2c6a60335ec
mohmmadaslam <mohmmad.aslam@iitgn.ac.in>
2020-10-06 16:04:19 +0530

    changing

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ import logging
 from logging import Formatter, FileHandler
 from forms import *
 import os
+import pandas
 
 #----------------------------------------------------------------------------#
 # App Config.
